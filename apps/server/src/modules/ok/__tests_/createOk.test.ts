349f6b10d0fa932c24abc9d713917ed0927d50a4
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-07 20:47:04 -0300

    test (fix): fixed types for creteOk and getOk to support globalID field in back-end

--- apps/server/src/modules/ok/__tests_/createOk.test.ts
+++ apps/server/src/modules/ok/__tests_/createOk.test.ts
@@ -1,17 +1,19 @@
 import { expect, test } from 'vitest'
 import request, { gql } from 'graphql-request'
-import { NexusGenObjects } from '../../../../nexus-typegen'
+import { NexusGenFieldTypes } from '../../../../nexus-typegen'
 
 test('should create ok', async() => {
     const mutation = gql`
         mutation okMutation($value: Boolean!) {
             ok(value: $value) {
                 value
+                id
             }
         }
     `
     
-    const result = await request<{ok: NexusGenObjects['ok']}>('http://localhost:4000/graphql', mutation, {value: true})
+    const result = await request<{ok: NexusGenFieldTypes['ok']}>('http://localhost:4000/graphql', mutation, {value: true})
 
     expect(result.ok.value).toBe(true)
+    expect(result.ok.id).toBeTypeOf("string")
 })
