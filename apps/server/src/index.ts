63dc167dca8ce145c5538626b16b9d6f836534c9
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-07 18:01:31 -0300

    feat (http): created basic koajs plus graphql server setup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello World!')
+import Koa from 'koa'
+import Router from '@koa/router'
+import { graphqlHTTP } from 'koa-graphql'
+import cors from '@koa/cors'
+import { GraphQLBoolean, GraphQLObjectType, GraphQLSchema } from 'graphql'
+
+const app = new Koa()
+const router = new Router()
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Query',
+        description: 'query root',
+        fields: () => ({
+            ok: { type: GraphQLBoolean, resolve: () => true }
+        })
+    })
+})
+
+router.all('/graphql',
+    graphqlHTTP({
+        schema,
+        context: {},
+        graphiql: true
+    })
+)
+
+app
+    .use(cors({ origin: '*' }))
+    .use(router.routes())
+    .use(router.allowedMethods())
+
+const PORT = 4000
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/graphql`))
