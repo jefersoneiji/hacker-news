0b4a22f9f1fe274cf4674acff48461b82ca77e47
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-08 11:37:01 -0300

    feat (router)!: created router setup for front-end

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { RelayProvider } from './relay/provider.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RelayProvider>
-      <App />
+      <RouterProvider router={router} />
     </RelayProvider>
   </React.StrictMode>,
 )
