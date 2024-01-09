bd0f09c49c7abb29afc87642c9124eee51eddd50
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-09 13:44:38 -0300

    test (user): created user plus with basic info

---
+++ apps/server/src/modules/user/__tests__/findUser.test.ts
@@ -0,0 +1,16 @@
+import request, { gql } from "graphql-request";
+import { expect, test } from "vitest";
+import { NexusGenFieldTypes } from "../../../../nexus-typegen";
+
+test('should thrown error when user is not found', async () => {
+    const query = gql`
+        query getUser {
+            user {
+                username
+            }
+        }
+    `
+    const result = request<NexusGenFieldTypes['Query']>('http://localhost:4000/graphql', query)
+
+    await expect(result).rejects.toThrowError('user not found')
+})
