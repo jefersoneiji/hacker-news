40fa7298ba3024ba997128df1162f9e2aeb062a0
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-09 17:16:57 -0300

    test (user): added e-mail field to user

--- apps/server/src/modules/user/__tests__/createUser.test.ts
+++ apps/server/src/modules/user/__tests__/createUser.test.ts
@@ -4,16 +4,18 @@ import { NexusGenFieldTypes } from "../../../../nexus-typegen";
 
 test('should return created user', async () => {
     const mutation = gql`
-    mutation createUser($username: String!){
-        signup(username: $username){
+    mutation createUser($username: String!, $email: String){
+        signup(username: $username, email: $email){
             username
             createdAt
+            email
         }
     }
     `
-    const args = { username: "johndoe" }
+    const args = { username: "johndoe", email: "john@email.com" }
     const result = await request<NexusGenFieldTypes['Mutation']>('http://localhost:4000/graphql', mutation, args)
 
     expect(result.signup.username).toEqual(args.username)
     expect(result.signup.createdAt).not.toBeNull()
+    expect(result.signup.email).toBeTypeOf('string')
 })
