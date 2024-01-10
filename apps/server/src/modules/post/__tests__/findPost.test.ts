6d3fd3285e5dbca5fea2f0d6e37c10ee65dd49b0
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-10 16:32:37 -0300

    test (post): created post display for page

---
+++ apps/server/src/modules/post/__tests__/findPost.test.ts
@@ -0,0 +1,28 @@
+import request, { gql } from "graphql-request";
+import { expect, test } from "vitest";
+import { NexusGenFieldTypes } from "../../../../nexus-typegen";
+
+test('should find post by its id', async () => {
+    const query = gql`
+        query getPost($postID: ID!){
+            findPost(postID: $postID){
+                title
+                createdAt
+                link
+                votedByLoggedUser
+                id
+            }
+        }
+    `
+
+    const result = await request<{findPost: NexusGenFieldTypes['post']}>(
+        'http://localhost:4000/graphql',
+        query,
+        { postID: "cG9zdDo2NTljOTNjNjg0MjEzODFjZjQyODc1ZWM=" }
+    )
+
+    expect(result.findPost.link).toBeTypeOf("string")
+    expect(result.findPost.votedByLoggedUser).toBeTypeOf("boolean")
+    expect(result.findPost.id).toBeTypeOf("string")
+    expect(result.findPost.createdAt).toBeTypeOf("string")
+})
