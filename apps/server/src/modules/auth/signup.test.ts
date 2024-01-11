9478994ccbf2f8dff884c5446aa7af67f05e0154
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-11 18:52:24 -0300

    feat (auth): created singup logic plus token containing userId logic. also migrated test from createUser to signup

---
+++ apps/server/src/modules/auth/signup.test.ts
@@ -0,0 +1,17 @@
+import request, { gql } from "graphql-request";
+import { expect, test } from "vitest";
+import { NexusGenFieldTypes } from "../../../nexus-typegen";
+
+test('should return created user', async () => {
+    const mutation = gql`
+        mutation createUser($username: String!, $password: String!){
+            signup(username: $username, password: $password){
+                token
+            }
+        }
+    `
+    const args = { username: "johndoe", password: "bestsweever" }
+    const result = await request<NexusGenFieldTypes['Mutation']>('http://localhost:4000/graphql', mutation, args)
+
+    expect(result.signup.token).toBeTypeOf('string')
+})
