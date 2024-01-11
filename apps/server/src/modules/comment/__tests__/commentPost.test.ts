d18f134e2f15b15259b2e1699a43ea94810b0643
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-11 15:05:33 -0300

    test (comment): added commenter to comments

--- apps/server/src/modules/comment/__tests__/commentPost.test.ts
+++ apps/server/src/modules/comment/__tests__/commentPost.test.ts
@@ -4,21 +4,26 @@ import { NexusGenFieldTypes } from "../../../../nexus-typegen";
 
 test('should comment a post', async () => {
     const mutation = gql`
-        mutation commentPost($postId: ID!, $comment: String!){
-            comment(postId: $postId, comment: $comment){
+        mutation commentPost($postId: ID!, $comment: String!,$userId: ID!){
+            comment(postId: $postId, comment: $comment, userId: $userId){
                 comment
                 createdAt
+                commenter {
+                    id
+                }
             }
         }
     `
 
     const args = {
         postId: "cG9zdDo2NTljOTNjNjg0MjEzODFjZjQyODc1ZWM=",
-        comment: "Action movies deliver an adrenaline rush with intense sequences, jaw-dropping stunts, and relentless excitement. From gripping plotlines to spectacular visuals, they keep us on the edge of our seats. These cinematic experiences are a thrilling escape, offering a rollercoaster of emotions and unforgettable moments of heroism."
+        comment: "Action movies deliver an adrenaline rush with intense sequences, jaw-dropping stunts, and relentless excitement. From gripping plotlines to spectacular visuals, they keep us on the edge of our seats. These cinematic experiences are a thrilling escape, offering a rollercoaster of emotions and unforgettable moments of heroism.",
+        userId: "dXNlcjo2NTllYmJjYzdjNGUyNzZjYmFhZDc0NmY="
     }
 
     const result = await request<{comment: NexusGenFieldTypes['comment']}>('http://localhost:4000/graphql', mutation, args)
 
     expect(result.comment.comment).toEqual(args.comment)
     expect(result.comment.createdAt).not.toBeNull()
+    expect(result.comment.commenter!.id).toEqual(args.userId)
 })
