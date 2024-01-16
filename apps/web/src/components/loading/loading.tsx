281312bc76f0a181dc0715425addd776c39a3918
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-16 18:29:53 -0300

    fix (suspense): added suspense for loading

---
+++ apps/web/src/components/loading/loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+    return (
+        <div>
+            <span>Loading...</span>
+        </div>
+    )
+}
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,17 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './global.css'
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import {  RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 
 import { RelayProvider } from './relay/provider.tsx'
 import { router } from './router.tsx'
+import { Loading } from './components/loading/loading.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RelayProvider>
-      <RouterProvider router={router} />
-    </RelayProvider>
+    <Suspense fallback={<Loading/>}>
+      <RelayProvider>
+        <RouterProvider router={router} />
+      </RelayProvider>
+    </Suspense>
   </React.StrictMode>,
 )
