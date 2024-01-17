e8d4cd56382cf0ae461c78ff12c645568daa3a56
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-17 14:49:03 -0300

    fix (comment)!: added form validation to  post-page

--- apps/web/src/modules/post/post-page.tsx
+++ apps/web/src/modules/post/post-page.tsx
@@ -1,6 +1,6 @@
 import { graphql } from "relay-runtime"
 import { useLazyLoadQuery, useMutation } from "react-relay"
-import { FormEvent, useState } from "react"
+import { useEffect, useState } from "react"
 
 import { Header } from "../../components/header/header"
 import { useShrink } from "../../utils/useShrink"
@@ -8,6 +8,9 @@ import type { postPageQuery as postPageQueryType } from "./__generated__/postPag
 import { PostDisplay } from "./display"
 import { Comment } from "./comment"
 import { useSearchParams } from "react-router-dom"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { jwtVerify } from "jose"
+import { toGlobalId } from "graphql-relay"
 
 const postPageQuery = graphql`
     query postPageQuery($postID: ID!){
@@ -25,29 +28,35 @@ const postPageMutation = graphql`
     }
 `
 
+type TComment = {
+    comment: string
+}
 export const PostPage = () => {
+    const { register, reset, handleSubmit, formState: { errors } } = useForm<TComment>()
     const [shrink] = useShrink()
 
-    const [searchParams] = useSearchParams()
-    const args = {
-        postID: searchParams.get('id')!,
-        userId: "dXNlcjo2NTlmMWY3MTVmOGFkMThkZTI5YWM1NGE="
-    }
-    const data = useLazyLoadQuery<postPageQueryType>(postPageQuery, args)
-    const [commitMutation] = useMutation(postPageMutation)
-    const [comment, setComment] = useState('')
+    const [userId, setUserId] = useState('')
+    useEffect(() => {
+        const token = localStorage.getItem('hn-token')!
+        jwtVerify(token, new TextEncoder().encode(import.meta.env.VITE_APP_SECRET)).then(res => setUserId(toGlobalId('post', res.payload.userId as string)))
+    }, [])
 
-    const onSubmit = (e: FormEvent<HTMLElement>) => {
-        e.preventDefault()
+    const [searchParams] = useSearchParams()
+    const postId = searchParams.get('id')!
+    const data = useLazyLoadQuery<postPageQueryType>(postPageQuery, { postID: postId })
+
+    const [commitMutation] = useMutation(postPageMutation)
+
+    const onSubmit: SubmitHandler<TComment> = (e: TComment) => {
 
         commitMutation({
             variables: {
-                postId: args.postID,
-                comment,
-                userId: args.userId
+                postId,
+                comment: e.comment,
+                userId
             },
             onCompleted() {
-                setComment('')
+                reset()
             }
         })
     }
@@ -56,15 +65,17 @@ export const PostPage = () => {
             <Header />
             <div className="container-fluid px-2 pt-2" style={{ backgroundColor: 'var(--spring-wood)' }}>
                 <PostDisplay post={data.findPost} />
-                <form onSubmit={onSubmit}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <textarea
                         rows={8}
                         cols={80}
+                        {...register('comment', { required: true, minLength: 20 })}
                         wrap="virtual"
                         className="mt-3"
-                        value={comment}
-                        onChange={e => setComment(e.target.value)}
                     />
+                    {errors.comment?.type === 'required' && <p className='text-danger' role='alert'>comment is required</p>}
+                    {errors.comment?.type === 'minLength' && <p className='text-danger' role='alert'>minimun lenght for comment is 20 characters</p>}
+
                     <div className="mt-3 pb-3">
                         <input type='submit' value='add comment' />
                     </div>
