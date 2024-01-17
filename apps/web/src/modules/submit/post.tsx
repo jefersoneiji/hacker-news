d0fd3170ba594ea8f04fc78a5a25bd21bfefc045
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-17 11:46:06 -0300

    fix (post)!: removed jwt to easy debugging in production environment

--- apps/web/src/modules/submit/post.tsx
+++ apps/web/src/modules/submit/post.tsx
@@ -32,7 +32,7 @@ export const Post = () => {
         const token = localStorage.getItem('hn-token')!
         console.log('vite_app_secret is: ',import.meta.env.VITE_APP_SECRET)
         console.log('token is: ',token)
-        jwtVerify(token, new TextEncoder().encode(import.meta.env.VITE_APP_SECRET)).then(res => setUserId(toGlobalId('post', res.payload.userId as string)))
+        // jwtVerify(token, new TextEncoder().encode(import.meta.env.VITE_APP_SECRET)).then(res => setUserId(toGlobalId('post', res.payload.userId as string)))
     }, [])
     
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
