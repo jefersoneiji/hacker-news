78dde21fe206d810d67c6e3a2f0e5d24c16fe804
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-18 16:32:17 -0300

    feat (comment): added feature of voting a comment

--- apps/server/src/modules/comment/commentModel.ts
+++ apps/server/src/modules/comment/commentModel.ts
@@ -1,14 +1,18 @@
 import mongoose, { Schema, Document } from "mongoose";
+import { user } from "../user/userModel";
 
 interface comment {
     comment: string; // String!
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     createdAt: any; // DateTime!
     postId: string; // ID!
+    commenter: user[] | null; // user
+    id: string; // ID!
 }
 const commentSchema = new Schema({
     postId: { type: Schema.Types.ObjectId, required: true },
-    comment: { type: String, required: true }
+    comment: { type: String, required: true },
+    voters: { type: [{ type: Schema.Types.ObjectId, ref: 'user' }] }
 }, {
     timestamps: { createdAt: true, updatedAt: false }
 })
