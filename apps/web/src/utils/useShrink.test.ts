6e9f59c92dd5614cd2ac7cab012807277e49b636
jefersoneiji <jeferson.eiji@icloud.com>
2024-01-24 11:29:48 -0300

    test (useShrink): use shrink value now it set during the first render

--- apps/web/src/utils/useShrink.test.ts
+++ apps/web/src/utils/useShrink.test.ts
@@ -2,6 +2,10 @@ import { renderHook, fireEvent, waitFor } from '@testing-library/react'
 
 import { useShrink } from "./useShrink"
 
+afterEach(() => {
+    window.innerWidth = 0
+})
+
 test('should return shrink equals false for window.innerWidh < 992', async () => {
     const { result } = renderHook(() => useShrink())
 
@@ -27,4 +31,13 @@ test('should return shrink equals true for window.innerWidh > 992', async () => 
     fireEvent(window, new Event('resize'))
 
     await waitFor(() => expect(result.current[0]).toBeTruthy())
+})
+
+test('should return shrink equals false for window.innerWidh > 992 even without resize', async () => {
+    const { result } = renderHook(() => useShrink())
+
+    window.innerWidth = 500
+
+    await waitFor(() => expect(result.current[0]).toBeFalsy())
+    
 })
